8b6281206ad5d6f87a58acfece284f2d777350dc
Andrew <andrewmoren93@gmail.com>
2023-01-11 22:01:54 +0300

    build helmet

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
+import Helmet from "../components/Helmet/Helmet";
+import { Container, Row, Col } from "reactstrap";
+
+import heroImg from "../assets/images/hero.png";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <Helmet title="Home">
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content"></div>
+            </Col>
+
+            <Col lg="6" md="6">
+              <div className="hero__img">
+                <img src={heroImg} alt="heroImg" className="w-100" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
