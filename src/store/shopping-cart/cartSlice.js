6ade4f9b130a4932ce96edb03538a0772d2b015a
Andrew <andrewmoren93@gmail.com>
2023-01-16 22:39:26 +0300

    create redux tolkit

---
+++ src/store/shopping-cart/cartSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  cartItems: [],
+  totalQuantity: 0,
+  totalAmount: 0,
+};
+
+const cartSlice = {
+  name: "cart",
+  initialState: initialState,
+
+  reducers: {
+    addItem(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.cartItems.find(
+        (item) => item.id === newItem.id
+      );
+      state.totalQuantity++;
+
+      if (!existingItem) {
+        state.cartItems.push({
+          id: newItem.id,
+          title: newItem.title,
+          image01: newItem.image01,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+      }
+    },
+  },
+};
