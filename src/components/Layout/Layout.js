f872736400cbfeb772638b948ee06ea01f1ca276
Andrew <andrewmoren93@gmail.com>
2023-01-17 17:11:11 +0300

    build cart && cartitem

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,14 @@ import React from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import Routers from "../../routes/Routers";
+import Carts from "../UI/cart/Carts";
 
 const Layout = () => {
   return (
     <div>
       <Header />
+
+      <Carts />
       <div>
         <Routers />
       </div>
---
+++ src/components/UI/cart/CartItem.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { ListGroupItem } from "reactstrap";
+import productImg from "../../../assets/images/product_01.1.jpg";
+
+import "../../../styles/cart-item.css";
+
+const CartItem = () => {
+  return (
+    <ListGroupItem className="border-0 cart__item">
+      <div className="cart__item-info d-flex gap-2">
+        <img src={productImg} alt="product_img" />
+
+        <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
+          <div>
+            <h6 className="cart__product-title">Burger</h6>
+            <p className="d-flex align-items-center gap-5 cart__product-price">
+              2x <span>$24.00</span>
+            </p>
+            <div className="d-flex align-items-center justify-content-between increase__decrease-btn">
+              <span className="increase__btn">
+                <i className="ri-add-line"></i>
+              </span>
+              <span className="quantity">2</span>
+              <span className="decrease__btn">
+                <i className="ri-subtract-line"></i>
+              </span>
+            </div>
+          </div>
+
+          <span className="delete__btn">
+            <i className="ri-close-line"></i>
+          </span>
+        </div>
+      </div>
+    </ListGroupItem>
+  );
+};
+
+export default CartItem;
