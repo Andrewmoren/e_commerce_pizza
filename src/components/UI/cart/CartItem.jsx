c856ef885d9357f5065cc2823de071f380c035fb
Andrew <andrewmoren93@gmail.com>
2023-01-18 21:09:05 +0300

    toogle cart

--- src/components/UI/cart/CartItem.jsx
+++ src/components/UI/cart/CartItem.jsx
@@ -2,25 +2,31 @@ import React from "react";
 import { ListGroupItem } from "reactstrap";
 import productImg from "../../../assets/images/product_01.1.jpg";
 
+import { useDispatch } from "react-redux";
+import { cartActions } from "../../../store/shopping-cart/cartSlice";
+
 import "../../../styles/cart-item.css";
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+  const { id, title, price, image01, quantity, totalPrice } = item;
+  const dispatch = useDispatch();
+
   return (
     <ListGroupItem className="border-0 cart__item">
       <div className="cart__item-info d-flex gap-2">
-        <img src={productImg} alt="product_img" />
+        <img src={image01} alt="product_img" />
 
         <div className="cart__product-info w-100 d-flex align-items-center gap-4 justify-content-between">
           <div>
-            <h6 className="cart__product-title">Burger</h6>
+            <h6 className="cart__product-title">{title}</h6>
             <p className="d-flex align-items-center gap-5 cart__product-price">
-              2x <span>$24.00</span>
+              {quantity}x <span>${totalPrice}</span>
             </p>
             <div className="d-flex align-items-center justify-content-between increase__decrease-btn">
               <span className="increase__btn">
                 <i className="ri-add-line"></i>
               </span>
-              <span className="quantity">2</span>
+              <span className="quantity">{quantity}</span>
               <span className="decrease__btn">
                 <i className="ri-subtract-line"></i>
               </span>
