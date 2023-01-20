c8fff7c4807d56870fc773b140f863c9aafb602c
Andrew <andrewmoren93@gmail.com>
2023-01-20 20:11:51 +0300

    build all foods

--- src/components/UI/common-section/CommonSection.jsx
+++ src/components/UI/common-section/CommonSection.jsx
@@ -7,7 +7,7 @@ const CommonSection = (props) => {
   return (
     <section className="common__section">
       <Container>
-        <h2>{props.title}</h2>
+        <h2 className="text-white">{props.title}</h2>
       </Container>
     </section>
   );
--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -2,10 +2,47 @@ import React from "react";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 
+import { Container, Row, Col } from "reactstrap";
+
+import products from "../assets/fake-data/products";
+import ProductCard from "../components/UI/product-card/ProductCard";
+
 const AllFoods = () => {
   return (
     <Helmet title="All Foods">
       <CommonSection title="All Foods" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6">
+              <div className="search__widget">
+                <input type="text" placeholder="Search...." />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+            <Col lg="6" md="6" sm="6" className="mb-5">
+              <div className="sorting__widget">
+                <select>
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically, A-Z</option>
+                  <option value="descending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+
+            {products.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
     </Helmet>
   );
 };
