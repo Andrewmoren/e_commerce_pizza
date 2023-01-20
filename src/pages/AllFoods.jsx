3c163bd2a50b89c6205e6912dc7ae4fdd2bff25e
Andrew <andrewmoren93@gmail.com>
2023-01-20 20:54:45 +0300

    create search

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 
@@ -10,6 +10,8 @@ import ProductCard from "../components/UI/product-card/ProductCard";
 import "../styles/all-foods.css";
 
 const AllFoods = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [productData, setProductData] = useState(products);
   return (
     <Helmet title="All Foods">
       <CommonSection title="All Foods" />
@@ -19,7 +21,12 @@ const AllFoods = () => {
           <Row>
             <Col lg="6" md="6" sm="6">
               <div className="search__widget d-flex align-items-center justify-content-between w-50">
-                <input type="text" placeholder="Search...." />
+                <input
+                  type="text"
+                  placeholder="Search...."
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)}
+                />
                 <span>
                   <i className="ri-search-line"></i>
                 </span>
@@ -37,11 +44,17 @@ const AllFoods = () => {
               </div>
             </Col>
 
-            {products.map((item) => (
-              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
-                <ProductCard item={item} />
-              </Col>
-            ))}
+            {productData
+              ?.filter((item) => {
+                if (searchTerm.value === " ") return item;
+                if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+                  return item;
+              })
+              .map((item) => (
+                <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                  <ProductCard item={item} />
+                </Col>
+              ))}
           </Row>
         </Container>
       </section>
