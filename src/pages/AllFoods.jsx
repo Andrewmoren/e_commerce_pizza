8c171534f3a6275a95cc8ea33dff38b86e1056fa
Andrew <andrewmoren93@gmail.com>
2023-01-21 12:27:43 +0300

    create pagination products: styled

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -9,15 +9,26 @@ import ProductCard from "../components/UI/product-card/ProductCard";
 import ReactPaginate from "react-paginate";
 
 import "../styles/all-foods.css";
+import "../styles/pagination.css";
 
 const AllFoods = () => {
   const [searchTerm, setSearchTerm] = useState("");
 
   const [pageNumber, setPageNumber] = useState(0);
-  const productPerPage = 8;
+
+  const searchedProduct = products.filter((item) => {
+    if (searchTerm.value === " ") return item;
+    if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+      return item;
+  });
+
+  const productPerPage = 12;
   const visitedPage = pageNumber * productPerPage;
-  const displayPage = products.slice(visitedPage, visitedPage + productPerPage);
-  const pageCount = Math.ceil(products.length / productPerPage);
+  const displayPage = searchedProduct.slice(
+    visitedPage,
+    visitedPage + productPerPage
+  );
+  const pageCount = Math.ceil(searchedProduct.length / productPerPage);
 
   const changePage = ({ selected }) => {
     setPageNumber(selected);
@@ -55,17 +66,11 @@ const AllFoods = () => {
               </div>
             </Col>
 
-            {displayPage
-              ?.filter((item) => {
-                if (searchTerm.value === " ") return item;
-                if (item.title.toLowerCase().includes(searchTerm.toLowerCase()))
-                  return item;
-              })
-              .map((item) => (
-                <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
-                  <ProductCard item={item} />
-                </Col>
-              ))}
+            {displayPage.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id} className="mb-4">
+                <ProductCard item={item} />
+              </Col>
+            ))}
 
             <div>
               <ReactPaginate
