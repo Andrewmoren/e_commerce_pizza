274ebca0164e76d82b11a5c8485cb086bde7adb0
Andrew <andrewmoren93@gmail.com>
2023-01-24 22:37:11 +0300

    create FoodDetails

--- src/pages/FoodDetails.jsx
+++ src/pages/FoodDetails.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+import products from "../assets/fake-data/products";
+import { useParams } from "react-router-dom";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
+
 const FoodDetails = () => {
   return <div>FoodDetails</div>;
 };
