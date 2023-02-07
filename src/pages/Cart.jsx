bf3c8475b02b7327c8b7faded3040cd2f381aa76
Andrew <andrewmoren93@gmail.com>
2023-02-07 19:09:51 +0300

    build Cart: table

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,11 +6,57 @@ import Helmet from "../components/Helmet/Helmet";
 import { useSelector } from "react-redux";
 import { Container, Row, Col } from "reactstrap";
 
+import "../styles/cart-page.css";
+
 const Cart = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
   return (
     <Helmet title="Cart">
-      <CommonSection title="Your Cart"></CommonSection>
+      <CommonSection title="Your Cart" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12">
+              {cartItems.length === 0 ? (
+                <h5 className="text-center">Your cart is empty</h5>
+              ) : (
+                <table className="table table-bordered">
+                  <thead>
+                    <tr>
+                      <th>Image</th>
+                      <th>Product Title</th>
+                      <th>Price</th>
+                      <th>Quantity</th>
+                      <th>Delete</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {cartItems.map((item) => (
+                      <Tr item={item} key={item.id} />
+                    ))}
+                  </tbody>
+                </table>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Helmet>
+  );
+};
+
+const Tr = (props) => {
+  const { image01, title, price, quantitty } = props.item;
+  return (
+    <tr>
+      <td className="text-center">{image01}</td>
+      <td className="text-center">{title}</td>
+      <td className="text-center">${price}</td>
+      <td className="text-center">{quantitty}px</td>
+      <td className="text-center">
+        <i className="ri-delete-bin-line"></i>
+      </td>
+    </tr>
   );
 };
 
