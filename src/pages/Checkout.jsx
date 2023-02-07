11bacef483f965cbc045b78bed0fe9fdfcca04f4
Andrew <andrewmoren93@gmail.com>
2023-02-07 19:56:56 +0300

    start build Checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
 
 const Checkout = () => {
-  return <div>Checkout</div>;
+  return (
+    <Helmet>
+      <CommonSection title="Checkout" />
+    </Helmet>
+  );
 };
 
 export default Checkout;
