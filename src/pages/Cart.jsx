c31a05b7b74f6477ad659d504a1737f51d7117e4
Andrew <andrewmoren93@gmail.com>
2023-02-07 18:35:00 +0300

    start buil Cart section

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import CommonSection from "../components/UI/common-section/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+
+import { useSelector } from "react-redux";
+import { Container, Row, Col } from "reactstrap";
+
 const Cart = () => {
-  return <div>Cart</div>;
+  return (
+    <Helmet title="Cart">
+      <CommonSection title="Your Cart"></CommonSection>
+    </Helmet>
+  );
 };
 
 export default Cart;
