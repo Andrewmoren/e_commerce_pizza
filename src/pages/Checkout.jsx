cb3733914955ac418f4da94aec1a38bd30fc02c8
Andrew <andrewmoren93@gmail.com>
2023-02-13 22:43:46 +0300

    checkout: add state && onsubmit

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Container, Row, Col } from "reactstrap";
 import CommonSection from "../components/UI/common-section/CommonSection";
@@ -7,10 +7,32 @@ import Helmet from "../components/Helmet/Helmet";
 import "../styles/checkout.css";
 
 const Checkout = () => {
+  const [enterName, setEnterName] = useState("");
+  const [enterEmail, setEnterEmail] = useState("");
+  const [enterNumber, setEnterNumber] = useState("");
+  const [enterCountry, setEnterCountry] = useState("");
+  const [enterCity, setEnterCity] = useState("");
+  const [postalCode, setPostalCode] = useState("");
+
+  const shippingInfo = [];
   const cartTotalAmout = useSelector((state) => state.cart.totalAmount);
   const shippingCost = 30;
 
   const totalAmount = cartTotalAmout + Number(shippingCost);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const userShippingAddress = {
+      name: enterName,
+      email: enterEmail,
+      phone: enterNumber,
+      country: enterCountry,
+      city: enterCity,
+      postalCode: postalCode,
+    };
+    shippingInfo.push(userShippingAddress);
+    console.log(shippingInfo);
+  };
+
   return (
     <Helmet title="Checkout">
       <CommonSection title="Checkout" />
@@ -19,27 +41,52 @@ const Checkout = () => {
           <Row>
             <Col lg="8" md="6">
               <h6 className="mb-4">Shipping Addres</h6>
-              <form className="checkout__form">
+              <form className="checkout__form" onSubmit={submitHandler}>
                 <div className="form__group">
-                  <input type="text" placeholder="Enter you name" />
+                  <input
+                    type="text"
+                    placeholder="Enter you name"
+                    required
+                    onChange={(e) => setEnterName(e.target.value)}
+                  />
                 </div>
                 <div className="form__group">
-                  <input type="email" placeholder="Enter your email" />
+                  <input
+                    type="email"
+                    placeholder="Enter your email"
+                    onChange={(e) => setEnterEmail(e.target.value)}
+                  />
                 </div>
                 <div className="form__group">
-                  <input type="number" placeholder="Phone number" />
+                  <input
+                    type="number"
+                    placeholder="Phone number"
+                    onChange={(e) => setEnterNumber(e.target.value)}
+                  />
                 </div>
                 <div className="form__group">
-                  <input type="text" placeholder="Country" />
+                  <input
+                    type="text"
+                    placeholder="Country"
+                    onChange={(e) => setEnterCountry(e.target.value)}
+                  />
                 </div>
                 <div className="form__group">
-                  <input type="text" placeholder="Enter your name" />
+                  <input
+                    type="text"
+                    placeholder="City"
+                    onChange={(e) => setEnterCity(e.target.value)}
+                  />
                 </div>
                 <div className="form__group">
-                  <input type="number" placeholder="Postal code" />
+                  <input
+                    type="number"
+                    placeholder="Postal code"
+                    onChange={(e) => setPostalCode(e.target.value)}
+                  />
                 </div>
+                <button className="addToCart__btn">Payments</button>
               </form>
-              <button className="addToCart__btn">Payments</button>
             </Col>
             <Col lg="4" md="6">
               <div className="checkout__bill">
