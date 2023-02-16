dce1d1dc626a657d9eb522cc4167d92e11904e18
Andrew <andrewmoren93@gmail.com>
2023-02-16 23:55:51 +0300

    fix: start build login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -10,16 +11,20 @@ const Login = () => {
       <section>
         <Container>
           <Row>
-            <Col lg="6" md="6" sm="12">
-              <form action="" className="form">
+            <Col lg="6" md="6" sm="12" className="m-auto text-center">
+              <form action="" className="form mb-5">
                 <div className="form__group">
-                  <input type="text" placeholder="Email" />
+                  <input type="text" placeholder="Email" required />
                 </div>
 
                 <div className="form__group">
-                  <input type="password" placeholder="Password" />
+                  <input type="password" placeholder="Password" required />
                 </div>
+                <div className="addToCart__btn">Login</div>
               </form>
+              <Link to="/register">
+                Alredy have an account? Create an account
+              </Link>
             </Col>
           </Row>
         </Container>
