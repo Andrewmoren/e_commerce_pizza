45175f0afe14310896b4e97398bc11c831b1c76f
Andrew <andrewmoren93@gmail.com>
2023-02-16 23:52:37 +0300

    fix: start build login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
+import { Container, Row, Col } from "reactstrap";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <Helmet title="Login">
+      <CommonSection title="Login" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <form action="" className="form">
+                <div className="form__group">
+                  <input type="text" placeholder="Email" />
+                </div>
+
+                <div className="form__group">
+                  <input type="password" placeholder="Password" />
+                </div>
+              </form>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Login;
