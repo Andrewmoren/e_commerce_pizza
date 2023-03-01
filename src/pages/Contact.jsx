d08a2215b63a9b14d21bd4803cd01d0776fe23da
Andrew <andrewmoren93@gmail.com>
2023-03-01 20:22:18 +0300

    create contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { Container } from "reactstrap";
+import Helmet from "../components/Helmet/Helmet";
+import CommonSection from "../components/UI/common-section/CommonSection";
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <Helmet title="Contact">
+      <CommonSection title="Contact">
+        <section>
+          <Container>
+            <Row>
+              <Col lg="3" md="3"></Col>
+            </Row>
+          </Container>
+        </section>
+      </CommonSection>
+    </Helmet>
+  );
 };
 
 export default Contact;
